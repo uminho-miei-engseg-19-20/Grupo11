ea025626b3fddb0cf2353145a92843a20400b8be
PedroFreitas90 <pedrofreitas1410@gmail.com>
2020-03-16 18:27:03 +0000

    p1.1

---
+++ TP4/p1/main.js
@@ -0,0 +1,78 @@
+// Exp 1.1 - KoreCoin - Blockchain
+
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    
+    constructor(index,timestamp,data,previousHash=''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+
+        this.hash = '';
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+
+}
+
+class Blockchain {
+    
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0,this.creatingNewTimestamp(), "Bloco inicial da koreCoin", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(b){
+        b.previousHash = this.getLatestBlock().hash; //Pega na hash do último bloco da blockchain
+        b.hash = b.calculateHash();
+        this.chain.push(b);
+    }
+
+    isChainValid(){
+        for(let i = 1 ; i < this.chain.length ; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if (currentBlock.previousHash !== previousBlock.hash)
+                return false;
+            if (currentBlock.hash !== currentBlock.calculateHash())
+                return false;
+        }
+        return true;
+    }
+
+    creatingNewTimestamp(){
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+        return dateTime;
+    }
+}
+
+let koreCoin = new Blockchain();
+
+koreCoin.addBlock(new Block (1, "01/01/2018", {amount: 20}));
+koreCoin.addBlock(new Block (2, "02/01/2018", {amount: 40}));
+koreCoin.addBlock(new Block (3, "02/01/2018", {amount: 40}));
+
+
+console.log(JSON.stringify(koreCoin, null, 4));
+
+console.log('Is Blockchain valid? ' + koreCoin.isChainValid());
+
+console.log('Vamos mudar valores da blockchain...');
+koreCoin.chain[1].data = {amount: 100}; //Pego no 2º block da blockchain e altero os dados.
+console.log('Is Blockchain valid? ' + koreCoin.isChainValid());
+koreCoin.chain[1].hash = koreCoin.chain[1].calculateHash();
+console.log('Is Blockchain valid? ' + koreCoin.isChainValid());
